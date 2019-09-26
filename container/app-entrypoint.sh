bd9a4a814bc7ed8766e4a5b7eb0324efbf391d0c
Dave Williams <dave@dave.io>
2019-09-26 22:57:48 +0100

    run custom steps twice

--- container/app-entrypoint.sh
+++ container/app-entrypoint.sh
@@ -6,10 +6,24 @@
 print_welcome_page
 
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
-  rm -rf /bitnami/mediawiki/skins/chameleon
-  rm -rf /opt/bitnami/mediawiki/composer.local.json
+  sed -i 's/^\$wgDefaultSkin.*$//g' /bitnami/mediawiki/LocalSettings.php
+  sed -i 's/^wfLoadSkin.*$//g' /bitnami/mediawiki/LocalSettings.php
+  sed -i 's/^\$wgMetaNamespace.*$//g' /bitnami/mediawiki/LocalSettings.php
+  echo >> /bitnami/mediawiki/LocalSettings.php
+  echo "# wiki.dave.io additional config" >> /bitnami/mediawiki/LocalSettings.php
+  echo >> /bitnami/mediawiki/LocalSettings.php
+  cat /additional-config.php >> /bitnami/mediawiki/LocalSettings.php
+  cp /composer.json /opt/bitnami/mediawiki/composer.json
+  cp /composer.local.json /opt/bitnami/mediawiki/composer.local.json
+  cp -r /theme /bitnami/mediawiki/skins/chameleon
+  (
+    cd /opt/bitnami/mediawiki
+    composer update
+  )
+  # upstream
   . /mediawiki-init.sh
   nami_initialize apache php mysql-client mediawiki
+  # /upstream
   sed -i 's/^\$wgDefaultSkin.*$//g' /bitnami/mediawiki/LocalSettings.php
   sed -i 's/^wfLoadSkin.*$//g' /bitnami/mediawiki/LocalSettings.php
   sed -i 's/^\$wgMetaNamespace.*$//g' /bitnami/mediawiki/LocalSettings.php
