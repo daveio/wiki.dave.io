2e3639649090eaea1f6ca565db7fa8e320f85928
Dave Williams <dave@dave.io>
2019-09-27 16:06:31 +0100

    enable x-forwarded-for

--- container/additional-config.php
+++ container/additional-config.php
@@ -3,3 +3,6 @@ wfLoadSkin( 'chameleon' );
 $egChameleonLayoutFile= __DIR__ . '/skins/chameleon/layouts/clean.xml';
 $wgArticlePath='/wiki/$1';
 $wgMetaNamespace = "Self";
+$wgUseSquid = true;
+$wgSquidServers = array();
+$wgSquidServers[] = "127.0.0.1";
